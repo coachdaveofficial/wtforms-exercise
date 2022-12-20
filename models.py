1ac7a948accda1e7261b3481fb6fef319153a096
David Falson <david@falson.net>
2022-12-19 20:03:26 -0800

    created Pet model

---
+++ models.py
@@ -0,0 +1,22 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Connect to database."""
+    db.app = app
+    db.init_app(app)
+
+
+class Pet(db.model):
+
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    species = db.Column(db.String(50), nullable=False)
+    photo_url = db.Column(db.String(), nullable=True, default='https://thecontemporarypet.com/wp-content/themes/contemporarypet/images/default.png')
+    age = db.Column(db.Integer, nullable=True)
+    notes = db.Column(db.String(250), nullable=True)
+    available = db.Column(db.Boolean, unique=False, default=True)
