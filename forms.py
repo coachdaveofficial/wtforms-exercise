9cfc548a24f4bae5a0f5024daf68517d8ef055f7
David Falson <david@falson.net>
2022-12-21 10:18:41 -0800

    added validation to name, species, and age

--- forms.py
+++ forms.py
@@ -1,18 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms import StringField, IntegerField, ValidationError
+from wtforms.validators import InputRequired, Optional, NumberRange, URL, EqualTo
+
+def pet_check(form, field):
+    if field.data.lower() not in ['cat', 'dog', 'porcupine']:
+        raise ValidationError('Species must be either cat, dog, or porcupine')
 
 class PetForm(FlaskForm):
     """Form for adding/editing Pet."""
 
     name = StringField("Name",
-                       validators=[InputRequired()])
+                       validators=[InputRequired(message="Pet name cannot be blank."), pet_check])
     species = StringField("Species",
-                        validators=[InputRequired()])
+                        validators=[InputRequired(message="Species cannot be blank")])
     photo_url = StringField("Profile Picture",
-                        validators=[Optional()])
+                        validators=[Optional(), URL()])
     age = IntegerField("Age",
-                        validators=[Optional()])
+                        validators=[Optional(), NumberRange(max=30)])
     notes = StringField("Notes",
                         validators=[Optional()])
         
